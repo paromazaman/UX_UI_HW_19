ff5ef23b1faf7213c4303726df6d6f28af8518de
paromazaman <146149540+paromazaman@users.noreply.github.com>
2023-11-15 23:06:06 -0800

    animation addeed

--- js/index.js
+++ js/index.js
@@ -1,4 +1,29 @@
-console.log("JS File Loaded!");
-$("#heroImg").on("click", function(){
-    console.log("YOU CLICKED ON #heroImg");
+console.log("js file loaded!");
+
+$(document).on("mouseover", ".profPic",function(){
+    var tmpAnimation = 0;
+    var element = $(".profPic");
+
+    
+    
+
+    $({degrees: tmpAnimation - 360}).animate({degrees: tmpAnimation}, {
+
+        duration: 2000,
+
+        step: function(now) {
+
+            element.css({
+
+                transform: 'rotate(' + now + 'deg)'
+
+            });
+
+        }
+
+    });
+});
+
+$(document).on("click","img", function(){
+    console.log("YOU CLICKED ON img");
 });
